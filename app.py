15dacadf365678a3f7c10867c58e1a852b8baa2f
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2024-09-02 22:05:17 +0100

    fix 504 issues

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ from flask import Flask, jsonify
 
 load_dotenv()
 
+data = pd.read_csv('main_data.csv')
+cv = CountVectorizer()
+countMatrix = cv.fit_transform(data['comb'])
+similarity = cosine_similarity(countMatrix)
+
+app = Flask(__name__, static_folder='movie-recommender-app/build',
+            static_url_path='/')
+CORS(app)
+
 def createSimilarity():
     
     data = pd.read_csv('main_data.csv') # reading the dataset
@@ -19,37 +28,24 @@ def createSimilarity():
 
 
 def getAllMovies():
-    data = pd.read_csv('main_data.csv')
     return list(data['movie_title'].str.capitalize())
 
 def Recommend(movie):
     movie = movie.lower()
-    try:
-        data.head()
-        similarity.shape
-    except:
-        (data, similarity) = createSimilarity()
     if movie not in data['movie_title'].unique():
         return 'Sorry! The movie you requested is not present in our database.'
     else:
         movieIndex = data.loc[data['movie_title'] == movie].index[0]
         lst = list(enumerate(similarity[movieIndex]))
         lst = sorted(lst, key=lambda x: x[1], reverse=True)
-        lst = lst[1:20]  # excluding first item since it is the requested movie itself and taking the top20 movies
-        movieList = []
-        for i in range(len(lst)):
-            a = lst[i][0]
-            movieList.append(data['movie_title'][a])
+        lst = lst[1:20]  # excluding first item since it is the requested movie itself and taking the top 20 movies
+        movieList = [data['movie_title'][i[0]] for i in lst]
         return movieList
 
 def getRandomMovies():
-    data = pd.read_csv('main_data.csv') # reading the dataset
-    random_movies = random.sample(list(data['movie_title']), 10) # select 10 random movies
+    random_movies = random.sample(list(data['movie_title']), 10)  # Select 10 random movies
     return random_movies
 
-app = Flask(__name__, static_folder='movie-recommender-app/build',
-            static_url_path='/')
-CORS(app)
 
 @app.route('/api/movies', methods=['GET'])
 @cross_origin()
@@ -75,18 +71,18 @@ def serve():
 
 @app.route('/api/similarity/<name>')
 @cross_origin()
-def similarity(name):
-    movie = name
-    recommendations = Recommend(movie)
-    if type(recommendations) == type('string'):
+def similarity_endpoint(name):
+    recommendations = Recommend(name)
+    if isinstance(recommendations, str):
+        # If it's an error message (string), split by '---'
         resultArray = recommendations.split('---')
-        apiResult = {'movies': resultArray}
-        return jsonify(apiResult)
     else:
+        # If it's a list of movie names
         movieString = '---'.join(recommendations)
         resultArray = movieString.split('---')
-        apiResult = {'movies': resultArray}
-        return jsonify(apiResult)
+    
+    apiResult = {'movies': resultArray}
+    return jsonify(apiResult)
 
 
 @app.errorhandler(404)
